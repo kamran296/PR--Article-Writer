e9cbc24e828522721b0d5f88ee9e25f378203014
Kamran Khot <kamran29khot@gmail.com>
2024-01-25 20:24:53 +0530

    api changed

--- frontend/src/pages/Form/Form.jsx
+++ frontend/src/pages/Form/Form.jsx
@@ -40,7 +40,7 @@ const Form = () => {
     console.log("Generating article");
     try {
       const response = await fetch(
-        "http://15.206.166.198/api/v1/ArticelWriter/articleForm",
+        "http://localhost:5000/api/v1/ArticelWriter/articleForm",
         {
           method: "POST",
           headers: {
