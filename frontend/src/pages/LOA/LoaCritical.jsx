50df68e706c125fe3d9424c5590444c53bc26284
Kamran Khot <kamran29khot@gmail.com>
2024-03-20 11:47:41 +0530

    build

--- frontend/src/pages/LOA/LoaCritical.jsx
+++ frontend/src/pages/LOA/LoaCritical.jsx
@@ -37,8 +37,8 @@ const LoaCritical = ({ type }) => {
     console.log("Generating article");
     try {
       const response = await fetch(
-        "http://15.206.166.198/api/v1/ArticelWriter/loaForm",
-        // "http://localhost:5000/api/v1/ArticelWriter/loaForm",
+        // "http://15.206.166.198/api/v1/ArticelWriter/loaForm",
+        "http://localhost:5000/api/v1/ArticelWriter/loaForm",
         {
           method: "POST",
           headers: {
