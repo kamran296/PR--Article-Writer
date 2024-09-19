e876e68033f4fc301e12236c591148c6700697b5
Omkar-Lakhe <104200749+Omkar-Lakhe@users.noreply.github.com>
2024-09-19 12:35:51 +0530

    glassdoor aggrigator

--- Backend/controllers/REA/aggregatorController.js
+++ Backend/controllers/REA/aggregatorController.js
@@ -1,54 +1,10 @@
-const axios = require("axios");
+const axios = require('axios');
 
 exports.getAggregatedSalary = async (req, res) => {
-  const { jobTitle, location } = req.body;
+    const { jobTitle, location } = req.body;
 
-  if (!jobTitle || !location) {
-    return res
-      .status(400)
-      .send({ error: "jobTitle and location are required parameters" });
-  }
-
-  try {
-    // Define the API calls
-    console.log("inside aggregrator");
-    const api1 = axios.post(
-      "https://www.internal.cachelabs.io/api/salary/salary",
-      {
-        jobTitle,
-        location,
-      }
-    );
-    const api2 = axios.post(
-      "https://www.internal.cachelabs.io/api/indeed/get-salary-indeed",
-      { jobRole: jobTitle, location }
-    );
-    // const api3 = axios.post('https://www.internal.cachelabs.io/api/talent/search-salary-talent', { jobTitle, location });
-    // const api4 = axios.post('https://www.internal.cachelabs.io/api/monster/monster-search', { jobTitle, location });
-    // const api5 = axios.post('https://www.internal.cachelabs.io/api/levels/search-Levels', { jobTitle, location });
-
-    // Initialize responses and errors
-    const results = {
-      salaryCom: null,
-      indeed: null,
-      // talent: null,
-      // monster: null,
-      // levels: null,
-      errors: [],
-    };
-
-    // Execute API calls
-    const [response1, response2] = await Promise.allSettled([api1, api2]);
-
-    console.log("salary.com res", response1);
-    // Process each response
-    if (response1.status === "fulfilled") {
-      results.salaryCom = response1.value.data;
-    } else {
-      results.errors.push({
-        api: "Salary.com",
-        error: response1.reason.message,
-      });
+    if (!jobTitle || !location) {
+        return res.status(400).send({ error: 'jobTitle and location are required parameters' });
     }
 
     try {
@@ -102,31 +58,5 @@ exports.getAggregatedSalary = async (req, res) => {
         console.error('Error in fetching data from APIs:', error);
         res.status(500).send({ error: 'An error occurred while fetching salary data' });
     }
-
-    // if (response3.status === 'fulfilled') {
-    //     results.talent = response3.value.data.salaryValues;
-    // } else {
-    //     results.errors.push({ api: 'Talent.com', error: response3.reason.message });
-    // }
-
-    // if (response4.status === 'fulfilled') {
-    //     results.monster = response4.value.data.salaryData;
-    // } else {
-    //     results.errors.push({ api: 'Monster.com', error: response4.reason.message });
-    // }
-
-    // if (response5.status === 'fulfilled') {
-    //     results.levels = response5.value.data;  // Added levelsFyi processing
-    // } else {
-    //     results.errors.push({ api: 'Levels.fyi', error: response5.reason.message });
-    // }
-
-    // Return the results
-    res.json(results);
-  } catch (error) {
-    console.error("Error in fetching data from APIs:", error);
-    res
-      .status(500)
-      .send({ error: "An error occurred while fetching salary data" });
-  }
 };
+
